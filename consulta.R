09cd6712c4385e8a01f38f326b4578fa1e6aa903
Felipe Resende <feliperesendehq@outlook.com>
2017-03-13 19:41:03 -0300

    finalizando

--- consulta.R
+++ consulta.R
@@ -1,36 +1,29 @@
 library(RSQLite)
 library(plot3D)
 setwd("C:\\Users\\Felipe Resende\\Desktop\\R\\Analise Jogo\\datacollect")
-con = dbConnect(SQLite(), dbname="aidata.db")
+con = dbConnect(SQLite(), dbname="bancofinal.db")
 dbGetQuery(con,"Select * from info")
 
 data <- dbGetQuery(con,"Select 
 (distancia_x*distancia_x) + (distancia_y * distancia_y) as dist,
 count(*) as freq,
-(button_left*1 + button_right*2 + button_down*4 + button_up*8 + button_a*16 + button_b*32 + button_c*64) as command
-from info
+button_left, button_right, button_down, button_up, button_a, button_b, button_c
+from ia
 where dist not null
 group by distancia_x, distancia_y, button_up,button_down,button_left,button_right,button_a,button_b,button_c
 order by dist")
+
+data
 
 dist = sqrt(data[1])
 dist = c(t(dist))
 freq = data[2]
 freq = c(t(freq ))
-command = data[3]
-command = c(t(command))
-plot(dist,command)
-plot(dist,freq)
-plot(freq,command)
+allbutton = c(as.vector(t(data[,3:9])))
 
-z <- matrix(freq,length(dist),length(command))
+matriz = matrix(freq,length(dist),7)
 
-scatter3D(dist,command,freq)
-text3D(dist,command,freq)
+persp3D(c(1:7),c(1:length(dist)),t(matriz)*allbutton, border='black', xlab = "Botões",ylab = "Distância", zlab = "Frequência")
 
-scatter3D(dist, command, freq, phi = 45, theta = 45, bty = "g", 
-pch = 20, cex = 2, ticktype = "detailed")
-
-
-
+hist3D(c(1:7),c(1:length(dist)),t(matriz)*allbutton, border='black')
 
